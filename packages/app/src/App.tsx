1bff430064e7f15ae62b993c3139a65dd42767f5
imranahmad88pr147 <imranahmad88pr@yahoo.com>
2026-09-16 23:39:29 +0500

    Replaced catalog page with home page

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,86 +1,17 @@
 import { createApp } from '@backstage/frontend-defaults';
 
-import catalogPlugin from '@backstage/plugin-catalog/alpha';
-
 import { navModule } from './modules/nav';
 import { homeModule } from './modules/home';
 
-// import {
-//   OpenIdConnectApi,
-//   ProfileInfoApi,
-//   BackstageIdentityApi,
-//   SessionApi,
-// } from '@backstage/core-plugin-api';
-
-// import { OAuth2 } from '@backstage/core-app-api';
-
 import { SignInPageBlueprint } from '@backstage/plugin-app-react';
-
 import { SignInPage } from '@backstage/core-components';
 
-import {
-  createFrontendModule,
-} from '@backstage/frontend-plugin-api';
-
-// import {
-//   createApiRef,
-//   createFrontendModule,
-//   configApiRef,
-//   discoveryApiRef,
-//   oauthRequestApiRef,
-//   ApiBlueprint,
-// } from '@backstage/frontend-plugin-api';
+import { createFrontendModule } from '@backstage/frontend-plugin-api';
 
 import {
   entraAuthApiRef,
   entraAuthApi,
 } from './apis/authApis';
-
-// const entraAuthApiRef = createApiRef<
-//   OpenIdConnectApi &
-//     ProfileInfoApi &
-//     BackstageIdentityApi &
-//     SessionApi
-// >().with({
-//   id: 'auth.entra',
-// });
-
-
-// const entraAuthApi = ApiBlueprint.make({
-//   name: 'entra',
-//   params: {
-//     defineParams =>
-//       defineParams({
-//         api: entraAuthApiRef,
-
-//         deps: {
-//           discoveryApi: discoveryApiRef,
-//           oauthRequestApi: oauthRequestApiRef,
-//           configApi: configApiRef,
-//         },
-
-//         factory: ({ discoveryApi, oauthRequestApi, configApi }) =>
-//           OAuth2.create({
-//             configApi,
-//             discoveryApi,
-//             oauthRequestApi,
-
-//             environment: configApi.getOptionalString(
-//               'auth.environment',
-//             ),
-
-//             provider: {
-//               id: 'oidc',
-//               title: 'Microsoft Entra ID',
-//               icon: () => null,
-//             },
-
-//             defaultScopes: ['openid', 'profile', 'email', 'offline_access'],
-//           }),
-//       }),
-//   },
-// });
-
 
 const signInPage = SignInPageBlueprint.make({
   params: {
@@ -99,10 +30,8 @@ const signInPage = SignInPageBlueprint.make({
   },
 });
 
-
 export default createApp({
   features: [
-    catalogPlugin,
     navModule,
     homeModule,
 
